5d33a8dfbdafb3c02cc26bcc61a8344efb9f672d
Danilo Carrasco <danilo.carrasco.95@gmail.com>
2022-01-20 16:17:42 -0300

    Agregado Tooltips en iconos de la parte ¿Quienes somos?

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
-$(function(){
+$(function () {
 
-    $("a").click(function(){
+    // ScrollSmooth
+    $("a").click(function () {
         var numeral = this.hash;
 
         $("html, body").animate({
@@ -8,7 +9,16 @@ $(function(){
         }, 800)
     })
 
-    $(document).on("scroll", function(){
+
+    // Background en negro en el navbar cuando se hace scroll por primera vez
+    $(document).on("scroll", function () {
         $(".navbar").css('background-color', '#000');
     });
+})
+
+
+// Tooltips
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
 })
